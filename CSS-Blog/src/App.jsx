ce42650403f0500292c51ee55bd5738c1daf17af
IsaiahSama <zeldaplayergl@gmail.com>
2025-02-07 16:03:03 -0400

    Added Base Routing

--- CSS-Blog/src/App.jsx
+++ CSS-Blog/src/App.jsx
@@ -4,14 +4,21 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import HomePage from './pages/HomePage'
+import ProfilePage from "./pages/ProfilePage"
+import NewPostPage from "./pages/NewPostPage"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <HomePage />
-    </>
+    <BrowserRouter>
+    
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/post/new" element={<NewPostPage />} />
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
 
